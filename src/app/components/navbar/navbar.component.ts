4b7f5cd96357ff2903b28a7c96f5f55a72640498
Fernando Hidalgo Rosabal <fhidalgorosabal@gmail.com>
2025-12-05 16:44:56 -0300

    feat: Add Navbar component

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ThemeService } from '../../services/theme.service';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  private themeService = inject(ThemeService);
+  
+  public theme = this.themeService.theme;
+
+  scrollToSection(sectionId: string): void {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+  toggleTheme(): void {
+    this.themeService.toggleTheme();
+  }
+}
+
